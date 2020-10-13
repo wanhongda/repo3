b1ae785a1e9cd0e881b11691fd37e6e076a8b7ad
wanhongda <451546614@qq.com>
2020-10-13 20:36:05 +0800

    创建了第二个项目

---
+++ gitstudy2/src/hellogit2.java
@@ -0,0 +1,8 @@
+public class hellogit2 {
+    public static void main(String[] args) {
+        System.out.println("hello git");
+        System.out.println("hello git");
+        System.out.println("hello git");
+        System.out.println("hello git");
+    }
+}
